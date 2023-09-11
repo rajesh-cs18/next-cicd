3046ec2d78cd78c1c0e602b1531904fb40a04b07
rajesh-cs18 <raj20032003@gmail.com>
2023-09-11 10:31:36 +0500

    test build files

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,8 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  env: {
-    weburl: 'http://localhost:3000'
-  },
   e2e: {
+    baseUrl: 'http://localhost:3000',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,7 +1,6 @@
 describe('Greeting Component', () => {
   it('displays the greeting', () => {
-    const web_url = Cypress.env('weburl');
-    cy.visit(web_url);
+    cy.visit('/');
     cy.get('[data-cy=greeting]').contains('Hello, Next.js!');
   });
 });
